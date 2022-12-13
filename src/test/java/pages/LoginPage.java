7bddc5a8f8b76a49ed28146c5e6e8808daa3b229
olenadudka <helen.lady2311@gmail.com>
2022-12-13 11:04:38 -0500

    CucumberReview

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,23 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.CommonMethods;
+
+public class LoginPage extends CommonMethods {
+    @FindBy(xpath ="//*[@id='txtUsername']")
+    public WebElement usernameTextField;  //we give the name of that particular webelement
+
+    @FindBy(id="txtPassword")
+    public WebElement passwordTextField;
+
+    @FindBy(xpath="//*[@id='btnLogin']")
+    public WebElement loginButton;
+
+    @FindBy(xpath="//*[@id='spanMessage']")
+    public WebElement errorMessage;
+    public LoginPage(){
+        PageFactory.initElements(driver,this);
+    }
+}
---
+++ src/test/java/steps/PageInitializers.java
@@ -0,0 +1,10 @@
+package steps;
+
+import pages.LoginPage;
+
+public class PageInitializers {
+    public static LoginPage login;
+    public static void initializePageObjects(){
+        login=new LoginPage();
+    }
+}
